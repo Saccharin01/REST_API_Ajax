8d95b1ba42b4add595d809272a151f4995db355a
Saccharin01 <chousik01@gmail.com>
2024-06-14 15:50:58 +0900

    server open :port8080 module calling success

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ const {htmlTemplate, tagComponent,http,fs,requestHandler} = require('./Assembly.
 const XMLHttpRequest = require('xhr2')
 const xhr = new XMLHttpRequest()
 
-console.log(xhr)
-console.log(htmlTemplate)
-console.log(tagComponent)
-console.log(fs)
-console.log(requestHandler)
-let server = http.createServer((req,res)=>{})
+// console.log(xhr)
+// console.log(htmlTemplate)
+// console.log(tagComponent)
+// console.log(fs)
+// console.log(requestHandler)
+
+let server = http.createServer((req,res)=>{
+
+  if (req.method === "GET") {
+    console.log(req.url)
+    if (req.url === "/") {
+      res.writeHead(200, {"content-Type": "text/html"});
+      res.end(htmlTemplate());
+    }
+    requestHandler(req.url, "js", res,"application/javaScript")
+    requestHandler(req.url, "ico", res,"image/ico")
+    tagComponent('h1','환영합니다')
+  }
+
+})
+
+let PORT = 8080
+server.listen(PORT, console.log(`server running on http://localhost:${PORT}/`))
