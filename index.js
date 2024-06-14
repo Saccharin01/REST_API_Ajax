e8bc055e6c636c1e3c4b45902e0982f2e0be5686
Saccharin01 <chousik01@gmail.com>
2024-06-14 17:34:25 +0900

    module imbeding

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import {tagComponent} from "./tagComponent.js";
 const xhr = new XMLHttpRequest()
 let reqMethod = "GET"
 let apiKey = `eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDA1Mjg1NjQifQ.OVaDZZp4wwS1EpDMDlNUvAUgGSfXNbTXYUbMiqd-NGbaj5MeVzJaSVBwdr68MAU-Fg1_muH13MmMer6qgE8JL4NLciG_4hXg0gXOprZ-dc9nxMQ9zdkzgetRVKz1Pw2AUFgDP342Wolms5GdH9i97C-gcYm9EdnJQKj5FNcm0iycT1qxuJH8yPhD5Uew4wgeUEkFo1PSjbsX4f29Sy2xO2cj9lnPQI4ArPDyCL4cP41yxXR3rJ2Kx0B-egydyQfiCaCTuliqunx2JDAh9kJMZ_GmehkzBJ6eMAcEt9hSN6QhYrbql6c8Nne1tCpmFgph09oguOGzUAQu-pQKA5-DIQ`
@@ -13,8 +14,7 @@ const root = document.getElementById('root')
 root.style.height = '100vh'
 root.style.width = '100vw'
 root.style.display ="grid"
-root.style.gridTemplateRows = "repeat(3, 1fr)"
-root.style.gridTemplateColumns = "repeat(3, 1fr)"
+root.style.gridTemplateRows = "repeat(6, 1fr)"
 root.style.gap = "30px"
 // root.style.
 
@@ -36,12 +36,8 @@ xhr.addEventListener(`load`, ()=>{
       root.appendChild(div)
       div.style.backgroundColor = "bisque"
     });
+    let divChild = document.querySelectorAll(`#root > div`)
+    console.log(divChild)
     let container = []
-
-    test.map((ele) => {
-      
-    });
-    console.log(container)
-    
   }
 })
