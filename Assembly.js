1d0ddf65efc6af49694781c64a4b368f2f797ba1
Saccharin01 <chousik01@gmail.com>
2024-06-14 15:15:06 +0900

    Assembly codindate

--- Assembly.js
+++ Assembly.js
@@ -1,4 +1,7 @@
 const htmlTemplate = require('./js/template.js')
 const tagComponent = require('./js/tagComponent.js')
+const fs = require('fs')
+const http = require('http')
+const requestHandler = require('./js/requestHandler.js')
 
-module.exports = {htmlTemplate, tagComponent}
+module.exports = {htmlTemplate, tagComponent,http,fs,requestHandler}
