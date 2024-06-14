ffe9f0880bbf2d89afe1d6f281986c7a4ccd18c5
Saccharin01 <chousik01@gmail.com>
2024-06-14 16:16:49 +0900

    not compeleted

--- app.js
+++ app.js
@@ -12,15 +12,32 @@ let server = http.createServer((req,res)=>{
 
   if (req.method === "GET") {
     console.log(req.url)
+
     if (req.url === "/") {
       res.writeHead(200, {"content-Type": "text/html"});
-      res.end(htmlTemplate());
-    }
-    requestHandler(req.url, "js", res,"application/javaScript")
-    requestHandler(req.url, "ico", res,"image/ico")
-    tagComponent('h1','환영합니다')
-  }
+      res.write(
+        htmlTemplate(tagComponent('h1', 'hola'))
+      );
+    }else{
+      if (req.url === "/favicon.ico") {
+        res.writeHead(200, {"content-Type": "image.ico"});
+        fs.readFile()
+        res.write();
+        res.end()
+      }
+  
+      if(req.url.split(".")[1] === 'js'){
+        requestHandler(req.url, "js", res,"application/javaScript")
+      }
+      // if(req.url.split(".")[1] === 'ico'){
+      //   requestHandler(req.url, "ico", res,"image/ico")
+      // }
 
+    }
+
+    // res.write(tagComponent('h1', 'hola'))
+
+  }
 })
 
 let PORT = 8080
