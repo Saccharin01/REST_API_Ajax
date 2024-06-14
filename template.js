96a67b1e130106db5e046ac5ca65d32d346aa074
Saccharin01 <chousik01@gmail.com>
2024-06-14 14:48:11 +0900

    module inject : htmlTemplate
    to response first connection, use this module to payload

---
+++ template.js
@@ -0,0 +1,16 @@
+const htmlTemplate = function htmlTemplate(){
+  return`<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+<div id="root"></div>
+<script type = "module" src="core.js"></script>
+</body>
+</html>`;
+}
+
+module.exports = htmlTemplate
